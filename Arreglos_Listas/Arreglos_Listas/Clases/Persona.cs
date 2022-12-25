b0b99bc0bf9dd5c1f29d65881aae9cb517523d4e
damian visualStudio <damiandiezzilli@gmail.com>
2022-12-25 16:13:07 -0300

    terminó ejercicio 1; add Persona Class

---
+++ Arreglos_Listas/Arreglos_Listas/Clases/Persona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Clase3Proyect.Class
+{
+    internal class Persona
+    {
+        private string Nombre { get; set; }
+        private string Apellido { get; set; }      
+        private DateTime FechaNacimiento { get; set; }
+    
+        public Persona(string nombre, string apellido)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+
+        public Persona(string nombre, string apellido, DateTime fechaNacimiento)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            FechaNacimiento = fechaNacimiento;
+
+        }
+
+        public string NombreCompleto()
+        {
+            return base.ToString() + ' ' + Apellido;
+        }
+
+        public int Edad()
+        {
+            TimeSpan ts = new TimeSpan();
+            ts = DateTime.Today - FechaNacimiento;
+            DateTime edad = new DateTime(ts.Ticks);
+            return edad.Year - 1;
+        }
+    }
+}
+
+
+
+
+
+
